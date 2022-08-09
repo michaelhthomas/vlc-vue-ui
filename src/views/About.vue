5e46cf85326599350d2a72a5cc7381d4745c63bb
Michael Thomas <michaelhthomas@outlook.com>
2022-08-08 22:26:18 -0400

    chore: update icon on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,7 @@
     <v-col cols="12" sm="8" md="6" xl="4">
       <v-card class="about">
         <v-row align="center" justify="center">
-          <img
-            height="200px"
-            src="https://upload.wikimedia.org/wikipedia/commons/e/e6/VLC_Icon.svg"
-            class="mt-4"
-          />
+          <img height="200px" src="@/assets/logo.svg" class="mt-4" />
         </v-row>
         <v-card-title> VLC Media Player - Material Web Interface </v-card-title>
         <v-card-subtitle> Version {{ appVersion }} </v-card-subtitle>
